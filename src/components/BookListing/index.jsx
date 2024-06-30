e48cca510e33c28603274127d91ad3743926b536
Ivan_Savchuk <vanya.savchuk@gmail.com>
2024-06-30 23:14:21 +0100

    update

--- src/components/BookListing/index.jsx
+++ src/components/BookListing/index.jsx
@@ -3,10 +3,11 @@ import { Link } from "react-router-dom";
 export default function BookListing({id, title, author, image}) {
     return (
         <div>
+            <Link to={`/book/${id}`}>
             <h3>{title}</h3>
             <p>{author}</p>
             <img src={image} alt="" />
-            <Link to={`/book/${id}`}>View more</Link>
+            </Link>
         </div>
     )
 }
